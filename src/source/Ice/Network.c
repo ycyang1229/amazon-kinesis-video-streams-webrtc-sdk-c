14da2e114be314ec1051acb99c0f6d9f468b7313
HungTseLee <hungtselee@realtek.com>
2020-12-17 16:33:41 +0800

    update for new setting of lwip options

--- src/source/Ice/Network.c
+++ src/source/Ice/Network.c
@@ -321,18 +321,18 @@ STATUS getIpWithHostName(PCHAR hostname, PKvsIpAddress destIp)
     STATUS retStatus = STATUS_SUCCESS;
     INT32 errCode;
     PCHAR errStr;
-    /////////////////////////////////////
-    struct addrinfo {
-               int              ai_flags;
-               int              ai_family;
-               int              ai_socktype;
-               int              ai_protocol;
-               socklen_t        ai_addrlen;
-               struct sockaddr *ai_addr;
-               char            *ai_canonname;
-               struct addrinfo *ai_next;
-           };
-    ///////////////////////////////
+//    /////////////////////////////////////
+//    struct addrinfo {
+//               int              ai_flags;
+//               int              ai_family;
+//               int              ai_socktype;
+//               int              ai_protocol;
+//               socklen_t        ai_addrlen;
+//               struct sockaddr *ai_addr;
+//               char            *ai_canonname;
+//               struct addrinfo *ai_next;
+//           };
+//    ///////////////////////////////
     struct addrinfo *res, *rp;
     BOOL resolved = FALSE;
     struct sockaddr_in* ipv4Addr;
@@ -340,7 +340,8 @@ STATUS getIpWithHostName(PCHAR hostname, PKvsIpAddress destIp)
 
     CHK(hostname != NULL, STATUS_NULL_ARG);
 
-    errCode = lwip_getaddrinfo(hostname, NULL, NULL, &res);
+    errCode = getaddrinfo(hostname, NULL, NULL, &res);
+    //errCode = lwip_getaddrinfo(hostname, NULL, NULL, &res);
 
     if (errCode != 0) {
 #ifdef KVS_PLAT_ESP_FREERTOS
@@ -365,7 +366,8 @@ STATUS getIpWithHostName(PCHAR hostname, PKvsIpAddress destIp)
         }
     }
 
-    lwip_freeaddrinfo(res);
+    //lwip_freeaddrinfo(res);
+    freeaddrinfo(res);
 
     CHK_ERR(resolved, STATUS_HOSTNAME_NOT_FOUND, "could not find network address of %s", hostname);
 
