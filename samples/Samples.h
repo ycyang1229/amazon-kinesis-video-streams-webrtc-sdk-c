ffcbb9c8ede071664dd12543ef4e674531c46b18
ycyang <yc.yang1229@gmail.com>
2021-06-25 00:51:12 +0800

    re-open ecs.

--- samples/Samples.h
+++ samples/Samples.h
@@ -75,7 +75,7 @@ extern "C" {
 
 /* Uncomment the following line in order to enable IoT credentials checks in the provided samples */
 //#define IOT_CORE_ENABLE_CREDENTIALS 1
-//#define ECS_ENABLE_CREDENTIALS 1
+#define ECS_ENABLE_CREDENTIALS 1
 
 typedef enum {
     SAMPLE_STREAMING_VIDEO_ONLY,
